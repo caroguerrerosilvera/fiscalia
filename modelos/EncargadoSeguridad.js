d7487c57cc75114ed7226af818e5e54e8ec00b76
caroguerrerosilvera <48815940+caroguerrerosilvera@users.noreply.github.com>
2020-06-06 18:10:49 -0500

    Adicionar modulo login (#1)

---
+++ modelos/EncargadoSeguridad.js
@@ -0,0 +1,10 @@
+// const conexion = require('./conexion');
+
+function autenticar (usuario, contrasena) {
+    console.log('modelo', usuario, contrasena);
+    return true;
+}
+
+module.exports = {
+    autenticar
+};
