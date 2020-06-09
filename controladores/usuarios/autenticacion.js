c10e099a07829e084f8b96d5cf02e334d4c446f5
AlejandraFreite <66481037+AlejandraFreite@users.noreply.github.com>
2020-06-08 22:23:21 -0500

    Home (#5)
    * diseño vista home
    
    * vistasVisitas
    
    * diseño vista home
    
    * vistasVisitas
    
    Co-authored-by: caroguerrerosilvera <48815940+caroguerrerosilvera@users.noreply.github.com>

---
+++ controladores/usuarios/autenticacion.js
@@ -0,0 +1,9 @@
+const usuarioModelo = require('../../modelos/EncargadoSeguridad');
+
+function autenticar (usuario, contrasena) {
+    const respuesta = usuarioModelo.autenticar(usuario, contrasena);
+    console.log('respuesta', respuesta);
+
+module.exports = {
+    autenticar
+}
