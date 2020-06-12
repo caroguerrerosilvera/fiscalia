c0925509434ebcb022aa0581a4ab273479ab637f
caroguerrerosilvera <48815940+caroguerrerosilvera@users.noreply.github.com>
2020-06-11 22:21:21 -0500

    Fix the detenidos query

--- modelos/Detenido.js
+++ modelos/Detenido.js
@@ -42,26 +42,24 @@ function registrarEntrada(cedulaDetenido, idEncargadoSeguridad) {
 
 function listar() {
     return new Promise(function(resolve, reject) {  
-        const query = `SELECT * FROM (SELECT
-            detenido.cedulaDetenido,
-            detenido.nombreDetenido,
-            detenido.apellidoDetenido,
-            detenido.fechaDeNacimiento,
-            detenido.idCelda,
-            registroingreso.horaYFechaIngreso,
-            registrosalida.horaYFechaSalida,
-            delito.nombreDelito,
-            encargadoEntrada.nombreEncargado as encargadoIngreso,
-            encargadoSalida.nombreEncargado as encargadoSalida
-            FROM detenido
-            LEFT JOIN registroingreso ON registroingreso.cedulaDetenido = detenido.cedulaDetenido
-            LEFT JOIN registrosalida ON registrosalida.cedulaDetenido = detenido.cedulaDetenido
-            LEFT JOIN delitodetenido ON delitodetenido.cedulaDetenido = detenido.cedulaDetenido
-            LEFT JOIN delito ON delito.idDelito = delitodetenido.idDelito
-            LEFT JOIN encargadoseguridad as encargadoEntrada ON encargadoEntrada.idEncargadoSeguridad = registroingreso.idEncargadoSeguridad
-            LEFT JOIN encargadoseguridad as encargadoSalida ON encargadoSalida.idEncargadoSeguridad = registrosalida.idEncargadoSeguridad
-            ORDER BY registroingreso.horaYFechaIngreso) as detenidos
-            GROUP BY cedulaDetenido LIMIT 1`;
+        const query = `SELECT MAX(detenido.cedulaDetenido) AS cedula,
+        MAX(detenido.nombreDetenido) AS nombreDetenido,
+        MAX(detenido.apellidoDetenido) AS apellidoDetenido,
+        MAX(detenido.fechaDeNacimiento) AS fechaDeNacimiento,
+        MAX(detenido.idCelda) AS celda,
+        MAX(registroingreso.horaYFechaIngreso) AS horaYFechaIngreso,
+        MAX(registrosalida.horaYFechaSalida) AS horaYFechaSalida,
+        MAX(delito.nombreDelito) AS delito,
+        MAX(encargadoEntrada.nombreEncargado) as encargadoIngreso,
+        MAX(encargadoSalida.nombreEncargado) as encargadoSalida
+        FROM detenido
+        LEFT JOIN registroingreso ON registroingreso.cedulaDetenido = detenido.cedulaDetenido
+        LEFT JOIN registrosalida ON registrosalida.cedulaDetenido = detenido.cedulaDetenido
+        LEFT JOIN delitodetenido ON delitodetenido.cedulaDetenido = detenido.cedulaDetenido
+        LEFT JOIN delito ON delito.idDelito = delitodetenido.idDelito
+        LEFT JOIN encargadoseguridad as encargadoEntrada ON encargadoEntrada.idEncargadoSeguridad = registroingreso.idEncargadoSeguridad
+        LEFT JOIN encargadoseguridad as encargadoSalida ON encargadoSalida.idEncargadoSeguridad = registrosalida.idEncargadoSeguridad
+        GROUP BY detenido.cedulaDetenido`;
         conexion.query(query, function (error, results) {
             if (error) return reject(error);
             resolve(results);
